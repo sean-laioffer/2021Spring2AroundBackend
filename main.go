42159b3d1980787cdfc16195086b126734ccd3e3
sean-laioffer <sean@laioffer.com>
2021-04-05 04:38:58 +0000

    Lesson 26 Commit.

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"   
+	
+	"github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Println("hello world")
+    fmt.Println("started-service")
+
+    r := mux.NewRouter()
+    r.Handle("/upload", http.HandlerFunc(uploadHandler)).Methods("POST", "OPTIONS")
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
